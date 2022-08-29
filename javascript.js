e2cbd1e7534ac4edcb73ad1835fe014b97afda97
AZRAEL <amir.khan14290@gmail.com>
2022-08-29 16:23:18 +0530

    a

--- javascript.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("centered_nav");
-    if (x.className === "rc_nav") {
-        x.className += " responsive";
-    } else {
-        x.className = "rc_nav";
-    }
-}
